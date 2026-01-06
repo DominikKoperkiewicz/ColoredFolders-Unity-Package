ceb61b48106979c69accb421cba302bad119e7db
Dominik Koperkiewicz <dominik.koperkiewicz@gmail.com>
2026-01-06 12:14:51 +0100

    Change CreateAssetMenu order

--- Editor/ColoredFoldersConstants.cs
+++ Editor/ColoredFoldersConstants.cs
@@ -3,7 +3,7 @@ namespace ColoredFolders.Editor
     public static class ColoredFoldersConstants
     {
         public const string CREATE_ASSET_MENU_PATH = "Colored Folders/";
-        public const int CREATE_ASSET_MENU_ORDER = -10000;
+        public const int CREATE_ASSET_MENU_ORDER = 100;
         
     }
 }
